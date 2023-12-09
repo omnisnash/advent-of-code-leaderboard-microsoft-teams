5184bbd8fa2c02a1220dc4127a3a63cb99a5c5a8
Pavel Djundik <xPaw@users.noreply.github.com>
2023-12-09 19:28:47 +0200

    Use node's fetch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
  */
 require('dotenv').config()
 
-const fetch = require('node-fetch');
 const parseJsonLeaderboard = require('./leaderboard-parser');
 const generateTeamsMessageCardLeaderboard = require('./template-generator');
 
